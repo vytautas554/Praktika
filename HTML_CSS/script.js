05d1ca463f10dd2160351b4999eb92abb2521463
vytautas554 <vytautaskrapikas@gmail.com>
2021-03-04 20:32:21 +0200

    js pabandymai

---
+++ HTML_CSS/script.js
@@ -0,0 +1,21 @@
+// index
+function fontBigger () {
+    document.getElementById('bigger-font').style.fontSize = '50px';
+}
+
+function fontItalic () {
+    document.getElementById('bigger-font').style.fontStyle = 'italic';
+}
+
+function changeText () {
+    document.getElementById('change-text').innerHTML = 'Dešimtas pagal dydį kalnas';
+}
+
+var moutainsHeight = 8.848 + 8.091 + 7.871 + 7.751 + 7.570 + 7.495 + 7.439 + 7.388;
+var c = moutainsHeight.toFixed(3);
+
+document.querySelector('.b').innerHTML = c;
+// end of index
+
+// index1
+
